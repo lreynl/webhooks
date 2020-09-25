ac90619534485b85c43419c722e0d370dc89b91d
LR <lr177245@gmail.com>
2020-09-24 23:02:05 -0700

    made the thing

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-// index.js
-
-/**
- * Required External Modules
- */
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
-/**
- * App Variables
- */
 const app = express();
 const port = process.env.PORT || 8000;
 const server = require('http').createServer(app);
@@ -18,76 +10,81 @@ ws(app);
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://ler:mypw123@cluster0.jjlsi.mongodb.net/cluster0?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+let gitHubWh;
+MongoClient.connect(uri, { useUnifiedTopology: true }, (err, client) => {
+  if (err) return console.error(err);
+  console.log('Connected to Database!');
+  const db = client.db('webhooks');
+  gitHubWh = db.collection('github');
 });
 
+const pg = require('pg');
+
+const conString = "postgres://spitbtaw:L3SGm7L1H152GeOD5qlK2iBsV6ynltQU@lallah.db.elephantsql.com:5432/spitbtaw" 
+const pg_client = new pg.Client(conString);
+const pg_add = (str) => {
+  pg_client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    pg_client.query(`insert into mdb_keys (mdb_key) values ('${str}')`, (err, result) => {
+      if(err) {
+        return console.error('error running query', err);
+      }
+      console.log('row was added');
+      pg_client.end();
+    });
+  });
+}
 
 let events = require('events');
 let eventEmitter = new events.EventEmitter();
 let webhook = { val: "empty" };
-//const wss = new ws.Server({ server: server, path: '/wspath' });
-/**
- *  App Configuration
- */
-//app.set("views", path.join(__dirname, "views"));
-//app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "public")));
+
+const fetchDb = () => {
+  let test = gitHubWh.find().toArray();
+  test.then(arr => {
+    console.log("HELLO");
+    return arr;
+  }).then((arr) => {
+    console.log(arr);
+  	eventEmitter.emit('received', JSON.stringify(arr));
+  });
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.ws('/', (ws, req) => {
-  ws.send("Does this work?");
   ws.on('message', msg => {
-    ws.send(msg);
+    fetchDb();
   });
   ws.on('close', () => {
     console.log('WebSocket was closed');
   });
   eventEmitter.on('received', (body) => {
-    //let str = webhook.val;
   	ws.send(body);
   });
 });
 
-/**
- * Routes Definitions
- */
- /*
-wss.on('connection', function connection(ws) {
-  console.log("Somebody just connected");
-  ws.send('Who are you?');
-  ws.on('message', function incoming(message) {
-    console.log(`received ${message}`);
-  });
-});
-*/
-
 app.get('/', (req, res) => {
-  //res.status(200).send("Does this work?");	
-  //res.render("index", {title: "Home"});
-  res.sendFile('index.html');
+  res.sendFile('/public/index.html', { root: __dirname });
 });
 
 app.post('/', jsonParser, (req, res) => {
-  //console.log(req.body);
-  console.log(req.body);
-  //console.log(req.text());
-  //res.status(200).send("Does this work?");	
-  //res.status(200).send(req.body);
-  //res.send("testing testing");
-  let str = JSON.stringify(req.body);
-  eventEmitter.emit('received', str);
-  //webhook.val = req.body;
-  res.redirect(302,"index", {posts: req.body});
-  //res.sendStatus(302);
+  let body = req.body;
+  console.log(body);
+  //let d = gitHubWh.deleteMany({});//////////////////////////////////////////////////////////////
+  let entry = gitHubWh.insertOne(body);
+  var id = 'empty';
+  entry.then(db => {
+    id = String(db.insertedId);
+    pg_add(id);
+    return id;
+  }).then(id => {
+    eventEmitter.emit('received', JSON.stringify(body));
+  });
+  res.redirect(302,"index", {posts: body});
 });
-/**
- * Server Activation
- */
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://${port}`);	
